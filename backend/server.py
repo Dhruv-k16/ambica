d624074b227db1f6d729ac69619c82ee7ca47e04
Dhruv <kd161104@gmail.com>
2026-02-17 17:42:08 +0530

    Fix CORS for Vercel production

--- backend/server.py
+++ backend/server.py
@@ -435,7 +435,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-       allow_origins=[
+    allow_origins=[
         "https://ambica-sigma.vercel.app",
         "http://localhost:3000"
     ],
